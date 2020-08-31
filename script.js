3be67d908dab0f00d97296ded641cadf87b794d2
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-08-30 21:37:03 -0700

    style corrections

--- script.js
+++ script.js
@@ -67,129 +67,129 @@ function auditTime() {
     var hourEight = document.getElementById("hourEight").textContent;
     var testHourEight = parseInt(hourEight);
     if (testHourEight === currentHour) {
-        $("#eight").find("p").addClass("present");
+        $("#eight").addClass("present");
     }
     else if (testHourEight > currentHour) {
-        $("#eight").find("p").addClass("future");
+        $("#eight").addClass("future");
     }
     else if (testHourEight < currentHour) {
-        $("#eight").find("p").addClass("past");
+        $("#eight").addClass("past");
     };
 
     // Audit 9:00
     var hourNine = document.getElementById("hourNine").textContent;
     var testHourNine = parseInt(hourNine);
     if (testHourNine === currentHour) {
-        $("#nine").find("p").addClass("present");
+        $("#nine").addClass("present");
     }
     else if (testHourEight > currentHour) {
-        $("#nine").find("p").addClass("future");
+        $("#nine").addClass("future");
     }
     else if (testHourNine < currentHour) {
-        $("#nine").find("p").addClass("past");
+        $("#nine").addClass("past");
     };
 
     // Audit 10:00
     var hourTen = document.getElementById("hourTen").textContent;
     var testHourTen = parseInt(hourTen);
     if (testHourTen === currentHour) {
-        $("#ten").find("p").addClass("present");
+        $("#ten").addClass("present");
     }
     else if (testHourTen > currentHour) {
-        $("#ten").find("p").addClass("future");
+        $("#ten").addClass("future");
     }
     else if (testHourTen < currentHour) {
-        $("#ten").find("p").addClass("past");
+        $("#ten").addClass("past");
     };
 
     // Audit 11:00
     var hourEleven = document.getElementById("hourEleven").textContent;
     var testHourEleven = parseInt(hourEleven);
     if (testHourEleven === currentHour) {
-        $("#eleven").find("p").addClass("present");
+        $("#eleven").addClass("present");
     }
     else if (testHourEleven > currentHour) {
-        $("#eleven").find("p").addClass("future");
+        $("#eleven").addClass("future");
     }
     else if (testHourEleven < currentHour) {
-        $("#eleven").find("p").addClass("past");
+        $("#eleven").addClass("past");
     };
 
     // Audit 12:00
     var hourTwelve = document.getElementById("hourTwelve").textContent;
     var testHourTwelve = parseInt(hourTwelve);
     if (testHourTwelve === currentHour) {
-        $("#twelve").find("p").addClass("present");
+        $("#twelve").addClass("present");
     }
     else if (testHourTwelve > currentHour) {
-        $("#twelve").find("p").addClass("future");
+        $("#twelve").addClass("future");
     }
     else if (testHourTwelve < currentHour) {
-        $("#twelve").find("p").addClass("past");
+        $("#twelve").addClass("past");
     };
 
     // Audit 13:00
     var hourThirteen = document.getElementById("hourThirteen").textContent;
     var testHourThirteen = parseInt(hourThirteen);
     if (testHourThirteen === currentHour) {
-        $("#thirteen").find("p").addClass("present");
+        $("#thirteen").addClass("present");
     }
     else if (testHourThirteen > currentHour) {
-        $("#thirteen").find("p").addClass("future");
+        $("#thirteen").addClass("future");
     }
     else if (testHourThirteen < currentHour) {
-        $("#thirteen").find("p").addClass("past");
+        $("#thirteen").addClass("past");
     };
 
     // Audit 14:00
     var hourFourteen = document.getElementById("hourFourteen").textContent;
     var testHourFourteen = parseInt(hourFourteen);
     if (testHourFourteen === currentHour) {
-        $("#fourteen").find("p").addClass("present");
+        $("#fourteen").addClass("present");
     }
     else if (testHourFourteen > currentHour) {
-        $("#fourteen").find("p").addClass("future");
+        $("#fourteen").addClass("future");
     }
     else if (testHourFourteen < currentHour) {
-        $("#fourteen").find("p").addClass("past");
+        $("#fourteen").addClass("past");
     };
 
     // Audit 15:00
     var hourFifteen = document.getElementById("hourFifteen").textContent;
     var testHourFifteen = parseInt(hourFifteen);
     if (testHourFifteen === currentHour) {
-        $("#fifteen").find("p").addClass("present");
+        $("#fifteen").addClass("present");
     }
     else if (testHourFifteen > currentHour) {
-        $("#fifteen").find("p").addClass("future");
+        $("#fifteen").addClass("future");
     }
     else if (testHourFifteen < currentHour) {
-        $("#fifteen").find("p").addClass("past");
+        $("#fifteen").addClass("past");
     };
 
     // Audit 16:00
     var hourSixteen = document.getElementById("hourSixteen").textContent;
     var testHourSixteen = parseInt(hourSixteen);
     if (testHourSixteen === currentHour) {
-        $("#sixteen").find("p").addClass("present");
+        $("#sixteen").addClass("present");
     }
     else if (testHourSixteen > currentHour) {
-        $("#sixteen").find("p").addClass("future");
+        $("#sixteen").addClass("future");
     }
     else if (testHourSixteen < currentHour) {
-        $("#sixteen").find("p").addClass("past");
+        $("#sixteen").addClass("past");
     };
 
     // Audit 17:00
     var hourSeventeen = document.getElementById("hourSeventeen").textContent;
     var testHourSeventeen = parseInt(hourSeventeen);
     if (testHourSeventeen === currentHour) {
-        $("#seventeen").find("p").addClass("present");
+        $("#seventeen").addClass("present");
     }
     else if (testHourSeventeen > currentHour) {
-        $("#seventeen").find("p").addClass("future");
+        $("#seventeen").addClass("future");
     }
     else if (testHourSeventeen < currentHour) {
-        $("#seventeen").find("p").addClass("past");
+        $("#seventeen").addClass("past");
     };
 };
