0f9c289bb452d0ad684e82167cb2e20e18f3e0ea
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-09-30 16:35:48 -0700

    Updates from Tutoring Sessions

--- script.js
+++ script.js
@@ -33,163 +33,122 @@ $(".description").on("blur", "textarea", function() {
 
     $(this).replaceWith(taskP);
 
-    localStorage.setItem("tasks", JSON.stringify(text));
 
     auditTime();
-    saveTasks();
+    //saveTasks();
 });
-
-// Save Button
-$(".saveBtn").click(function () {
-    alert("Item Saved");
-    saveTasks();
-});
-
-//Save Tasks Function
-function saveTasks() {
-    localStorage.setItem("tasks", JSON.stringify(text));
-};
-
-// Load Tasks Function
-function loadTasks() {
-    JSON.parse(localStorage.getItem("tasks"))
-};
-loadTasks();
 
 
 // Define Audit Function
 function auditTime() {
+
+    // Helper Array
+    var helperArray = [
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17"
+    ]
     // Determine value of current hour
     var m = moment().format('H');
     var currentHour = parseInt(m)
+    console.log(currentHour);
+
+    //Identify container where rows will be created
+    var parent = document.getElementById("container")
+
+    for (i=0; i<9; i++) { 
+
+        console.log(helperArray[i]);
+        // Pull from array and convert to number
+        var testHour = parseInt(helperArray[i]);
+        var hourString = helperArray[i];
+        console.log(typeof testHour);
+        console.log(typeof hourString);
+
+        //Create Rows with vanilla Javascript
+        var row = document.createElement("div");
+        row.setAttribute("class", "row");
+
+        // Hour Display
+        var divOne = document.createElement("div");
+        divOne.setAttribute("class", "col-2 hour");
+        var hourSpan = document.createElement("span");
+        hourSpan.textContent = hourString + ":00";
+
+        //User Input for Tasks
+        var taskDescriptionDiv = document.createElement("div");
+        taskDescriptionDiv.setAttribute("class", "col-9 description");
+        var taskInput = document.createElement("input");
+        taskInput.setAttribute("placeholder", "Add task description here");
+        taskInput.setAttribute("id", ("userInput" + hourString));
     
-    // Audit 8:00
-    var hourEight = document.getElementById("hourEight").textContent;
-    var testHourEight = parseInt(hourEight);
-    if (testHourEight === currentHour) {
-        $("#eight").addClass("present");
-    }
-    else if (testHourEight > currentHour) {
-        $("#eight").addClass("future");
-    }
-    else if (testHourEight < currentHour) {
-        $("#eight").addClass("past");
-    };
 
-    // Audit 9:00
-    var hourNine = document.getElementById("hourNine").textContent;
-    var testHourNine = parseInt(hourNine);
-    if (testHourNine === currentHour) {
-        $("#nine").addClass("present");
-    }
-    else if (testHourEight > currentHour) {
-        $("#nine").addClass("future");
-    }
-    else if (testHourNine < currentHour) {
-        $("#nine").addClass("past");
-    };
+        //Blue Save Button
+        var saveButtonDiv = document.createElement("div");
+        saveButtonDiv.setAttribute("class", "col-1 saveBtn");
+        saveButtonDiv.setAttribute("id", ("saveButton" + hourString));
+        var saveIconSpan = document.createElement("span");
+        saveIconSpan.setAttribute("class", "oi oi-briefcase icon");
+        
 
-    // Audit 10:00
-    var hourTen = document.getElementById("hourTen").textContent;
-    var testHourTen = parseInt(hourTen);
-    if (testHourTen === currentHour) {
-        $("#ten").addClass("present");
-    }
-    else if (testHourTen > currentHour) {
-        $("#ten").addClass("future");
-    }
-    else if (testHourTen < currentHour) {
-        $("#ten").addClass("past");
-    };
+        // If row exists, audit time and assign a class (conditional)
+        if (testHour === currentHour) {
+            taskDescriptionDiv.setAttribute("class", "present");
+        }
+        else if (testHour > currentHour) {    
+            taskDescriptionDiv.setAttribute("class", "future");
+        } 
+        else if (testHour < currentHour) {
+            taskDescriptionDiv.setAttribute("class", "past");
+        }
 
-    // Audit 11:00
-    var hourEleven = document.getElementById("hourEleven").textContent;
-    var testHourEleven = parseInt(hourEleven);
-    if (testHourEleven === currentHour) {
-        $("#eleven").addClass("present");
-    }
-    else if (testHourEleven > currentHour) {
-        $("#eleven").addClass("future");
-    }
-    else if (testHourEleven < currentHour) {
-        $("#eleven").addClass("past");
-    };
+        // Append Hour Span to Hour Div and row
+        divOne.appendChild(hourSpan);
+        row.appendChild(divOne);
 
-    // Audit 12:00
-    var hourTwelve = document.getElementById("hourTwelve").textContent;
-    var testHourTwelve = parseInt(hourTwelve);
-    if (testHourTwelve === currentHour) {
-        $("#twelve").addClass("present");
-    }
-    else if (testHourTwelve > currentHour) {
-        $("#twelve").addClass("future");
-    }
-    else if (testHourTwelve < currentHour) {
-        $("#twelve").addClass("past");
-    };
+        //Append user input text to div and row
+        taskDescriptionDiv.appendChild(taskInput);
+        row.appendChild(taskDescriptionDiv);
 
-    // Audit 13:00
-    var hourThirteen = document.getElementById("hourThirteen").textContent;
-    var testHourThirteen = parseInt(hourThirteen);
-    if (testHourThirteen === currentHour) {
-        $("#thirteen").addClass("present");
-    }
-    else if (testHourThirteen > currentHour) {
-        $("#thirteen").addClass("future");
-    }
-    else if (testHourThirteen < currentHour) {
-        $("#thirteen").addClass("past");
-    };
+        //Append save button to div and row
+        saveButtonDiv.appendChild(saveIconSpan);
+        row.appendChild(saveButtonDiv);
 
-    // Audit 14:00
-    var hourFourteen = document.getElementById("hourFourteen").textContent;
-    var testHourFourteen = parseInt(hourFourteen);
-    if (testHourFourteen === currentHour) {
-        $("#fourteen").addClass("present");
+        //Append row to container
+        parent.appendChild(row);
     }
-    else if (testHourFourteen > currentHour) {
-        $("#fourteen").addClass("future");
-    }
-    else if (testHourFourteen < currentHour) {
-        $("#fourteen").addClass("past");
-    };
+}
 
-    // Audit 15:00
-    var hourFifteen = document.getElementById("hourFifteen").textContent;
-    var testHourFifteen = parseInt(hourFifteen);
-    if (testHourFifteen === currentHour) {
-        $("#fifteen").addClass("present");
-    }
-    else if (testHourFifteen > currentHour) {
-        $("#fifteen").addClass("future");
-    }
-    else if (testHourFifteen < currentHour) {
-        $("#fifteen").addClass("past");
-    };
+// Function to Save Tasks into Local Storage
+var saveTask = function(event) {
+    var tgt = event.target;
+    var presSib = tgt.previousSibling;
+    var textBox = presSib.firstChild;
+    var id = textBox.id;
+    var userInput = textBox.value;
+    
+    var storageItems = {id, userInput}
 
-    // Audit 16:00
-    var hourSixteen = document.getElementById("hourSixteen").textContent;
-    var testHourSixteen = parseInt(hourSixteen);
-    if (testHourSixteen === currentHour) {
-        $("#sixteen").addClass("present");
-    }
-    else if (testHourSixteen > currentHour) {
-        $("#sixteen").addClass("future");
-    }
-    else if (testHourSixteen < currentHour) {
-        $("#sixteen").addClass("past");
-    };
 
-    // Audit 17:00
-    var hourSeventeen = document.getElementById("hourSeventeen").textContent;
-    var testHourSeventeen = parseInt(hourSeventeen);
-    if (testHourSeventeen === currentHour) {
-        $("#seventeen").addClass("present");
-    }
-    else if (testHourSeventeen > currentHour) {
-        $("#seventeen").addClass("future");
-    }
-    else if (testHourSeventeen < currentHour) {
-        $("#seventeen").addClass("past");
-    };
-};
+    localStorage.setItem("WorkDayScheduler", JSON.stringify(storageItems));
+
+
+}
+
+// Function to Retrieve Tasks from Local Storage and Display to Page
+var retrieveTask = function() {
+    localStorage.getItem
+}
+
+// Event Listener for Save Button
+$(document).on("click", ".saveBtn", saveTask)
+
+//Run Retrive Task Function on page load
+//retrieveTask();
